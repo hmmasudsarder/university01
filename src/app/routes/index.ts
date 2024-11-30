a355b182849475457bdd22135c58df8ec8348c77
hmmasudsarder <hmmasudsardar@gmail.com>
2024-12-01 01:21:42 +0600

    added routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { UserRoutes } from '../modules/users/users.route';
+import { StudentRoutes } from '../modules/students/student.route';
+
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
