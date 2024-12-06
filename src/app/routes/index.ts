c634a3bf8db79b611078753f23b5d2fd51bf1d2e
hmmasudsarder <hmmasudsardar@gmail.com>
2024-12-06 11:22:23 +0600

    finish academicFaclty or testing successfully

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { UserRoutes } from '../modules/users/users.route';
 import { StudentRoutes } from '../modules/students/student.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 
 
@@ -19,6 +20,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculty',
+    route: AcademicFacultyRoutes,
   },
 ];
 
